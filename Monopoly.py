dbd6ff4bf703c9614e26e5d99a8b1f23a9a8ff53
Raj Reddy <rajreddy259@gmail.com>
2023-07-30 17:50:01 -0600

    submission commit

--- Monopoly.py
+++ Monopoly.py
@@ -30,7 +30,7 @@ bg = pygame.image.load('assets/black.jpg')
 player_img = pygame.image.load('assets/car.png')
 dice_img = pygame.image.load('assets/dice.png')
 
-# Reduce the size of the images
+# reduce the size of the images
 scaled_player_img = pygame.transform.scale(player_img, (player_img.get_width() // 2, player_img.get_height() // 2))
 scaled_dice_img = pygame.transform.scale(dice_img, (dice_img.get_width() // 2, dice_img.get_height() // 2))
 
@@ -146,45 +146,46 @@ class Player(pygame.sprite.Sprite):
         self.get_out_of_jail_cards = 0
         self.turns = 0
 
-# Create a Player object
+# create a Player object
 player = Player(432, 936 - 48)
 
-# Function to draw the player on the screen
+# function to draw the player on the screen
 def draw_player():
     screen.blit(player.image, player.rect)
 
 
 # Function to draw buttons on the screen
 def draw_buttons():
-    # Draw Dice button
+    # dice button
     roll_dice = pygame.draw.rect(screen, white, (50, 800, 100, 40))  # Width: 100, Height: 40
     draw_text("Roll Dice", font, (0, 0, 0), screen, roll_dice.centerx-40, roll_dice.centery-15)
 
-    # Draw Community Chest button
+    # Community Chest button
     cc_button = pygame.draw.rect(screen, white, (200, 800, 130, 40))  # Width: 130, Height: 40
     draw_text("C Chest", font, (0, 0, 0), screen, cc_button.centerx-55, cc_button.centery-15)
 
-    # Draw Chance button
+    # Chance button
     chance_button = pygame.draw.rect(screen, white, (600, 800, 100, 40))  # Width: 100, Height: 40
     draw_text("Chance", font, (0, 0, 0), screen, chance_button.centerx-35, chance_button.centery-15)
 
-# a draw text method to render text on a blank screen
+# draw text method to render text on a blank screen
 def draw_text(text, font, text_col, x, y):
     img = font.render(text, True, text_col)
     screen.blit(img, (x, y))
 
+# this function will place a gray box around the spaces and landings, which will copy the data onto the clipboard
 def draw_copy_area():
     copy_area = pygame.draw.rect(screen, (100, 100, 100), (350, 50, 300, 600))  # Adjust the dimensions as needed
     return copy_area
 
-# Function to draw text on the screen for the locations as they are a smaller size
+# function to draw text on the screen for the locations as they are a smaller size
 def draw_text(text, font, color, surface, x, y):
     lines = text.split('\n')
-    line_height = font.size("")[1]  # Get the height of a single line of text
+    line_height = font.size("")[1]  # get the height of a single line of text
     for i, line in enumerate(lines):
         text_obj = font.render(line, True, color)
         text_rect = text_obj.get_rect()
-        text_rect.topleft = (x, y + i * line_height)  # Adjust vertical spacing using line_height
+        text_rect.topleft = (x, y + i * line_height)  # adjust vertical spacing using line_height
         surface.blit(text_obj, text_rect)
 
 
@@ -211,69 +212,135 @@ for _ in range(1000):
                 running = False
             elif event.type == MOUSEBUTTONDOWN and event.button == 1:
                 if copy_area_rect.collidepoint(event.pos):
-                            # Copy the spaces and landings text to the clipboard
+                            # will copy the spaces and landings text to the clipboard
                             space_counts_text = "\n".join([f" {count}" for space, count in space_counts.items()])
                             pyperclip.copy(space_counts_text)
                 if 50 <= event.pos[0] <= 200 and 800 <= event.pos[1] <= 850:
-                    # while player.turns < 1000000: # Only roll the dice if the player is not in jail
-
-                       
+                    # while player.turns < 10000: # loop to test n turns, uncomment this to test different n values
 
                         die1 = random.randint(1, 6)
                         die2 = random.randint(1, 6)
                         total_moves = die1 + die2
 
-                        # Update player position and handle board wrap-around
+                        # update player position and handle board wrap-around
                         player.position = (player.position + total_moves) % len(locations)
 
-                        # Increment the number of moves
+                        # increment the number of moves
                         player.moves += total_moves
                         
-                        # Increment the number of turnsffffffff
+                        # increment the number of turns
                         player.turns += 1
-
+                        
+                        #location on the board and will increment the value
                         current_space = list(locations.keys())[player.position]
                         space_counts[current_space] += 1
 
-                        # Handle landing on Community Chest and Chance spaces
+                        # handle landing on Community Chest and Chance spaces
                         current_space = list(locations.keys())[player.position]
                         if "Community Chest" in current_space:
-                            # Draw a Community Chest card and process it
+                            # draw a Community Chest card and process it
                             card = cc_queue.popleft()
                             cc_queue.append(card)  # Put the card back to the bottom of the deck
                             print(f"Community Chest Card: {card}")
                             if "Go to Jail" in card:
-                                in_jail = True
-                                current_space = "In Jail/Just Visiting"
-                                space_counts[current_space] += 1
-                                if get_out_of_jail_cards > 0:
-                                    # Use the "Get Out of Jail Free" card to avoid staying in jail
-                                    get_out_of_jail_cards -= 1
-                                    in_jail = False
+                                if get_out_of_jail_cards > 1:
+                                 get_out_of_jail_cards -= 1
+                                 in_jail = False
+                            else:
+                                in_jail = False
+                                ''' The code below is for strategy B'''
+                                # in_jail = True
+                                # current_space = "In Jail/Just Visiting"
+                                # space_counts[current_space] += 1
+                                # die1 = random.randint(1, 6)
+                                # die2 = random.randint(1, 6)
+                                # if die1 == die2:
+                                #     # If doubles are rolled, get out of jail
+                                #     in_jail = False
+                                #     turns_in_jail = 0
+                                # else:
+                                #     turns_in_jail += 1
+
+                                # if turns_in_jail >= 3:
+                                #     # After three turns, assume the player pays the $50 fine and gets out of jail
+                                #     in_jail = False
+                                #     turns_in_jail = 0
+                                # if get_out_of_jail_cards > 0:
+                                #     # use the "Get Out of Jail Free" card to avoid staying in jail
+                                #     get_out_of_jail_cards -= 1
+                                #     in_jail = False
+                                
                                 
                                 
                         elif "Chance" in current_space:
-                            # Draw a Chance card and process it
+                            # draw a Chance card and process it
                             card = chance_queue.popleft()
-                            chance_queue.append(card)  # Put the card back to the bottom of the deck
+                            chance_queue.append(card)  # put the card back to the bottom of the deck
                             print(f"Chance Card: {card}")
                             if "Go to Jail" in card:
-                                in_jail = True
-                                current_space = "In Jail/Just Visiting"
-                                space_counts[current_space] += 1
-                                if get_out_of_jail_cards > 0:
-                                    # Use the "Get Out of Jail Free" card to avoid staying in jail
-                                    get_out_of_jail_cards -= 1
-                                    in_jail = False
+                                if get_out_of_jail_cards > 1:
+                                 get_out_of_jail_cards -= 1
+                                 in_jail = False
+                            else:
+                                in_jail = False
+                                '''The code below is for strategy B'''
+                                # in_jail = True
+                                # current_space = "In Jail/Just Visiting"
+                                # space_counts[current_space] += 1
+                                # die1 = random.randint(1, 6)
+                                # die2 = random.randint(1, 6)
+                                # if die1 == die2:
+                                #     # If doubles are rolled, get out of jail
+                                #     in_jail = False
+                                #     turns_in_jail = 0
+                                # else:
+                                #     turns_in_jail += 1
+
+                                # if turns_in_jail >= 3:
+                                #     # After three turns, assume the player pays the $50 fine and gets out of jail
+                                #     in_jail = False
+                                #     turns_in_jail = 0
+                                # if get_out_of_jail_cards > 0:
+                                #     # use the "Get Out of Jail Free" card to avoid staying in jail
+                                #     get_out_of_jail_cards -= 1
+                                #     in_jail = False
                                 
 
                         elif "Go To Jail!" in current_space:
-                            current_space = "In Jail/Just Visiting"
+                            if get_out_of_jail_cards > 1:
+                                 get_out_of_jail_cards -= 1
+                                 in_jail = False
+                            else:
+                                 in_jail = False
+                            
+                            '''The code below is for strategy B'''
+                            # in_jail = True
+                            # current_space = "In Jail/Just Visiting"
+                            # space_counts[current_space] += 1
+                            # die1 = random.randint(1, 6)
+                            # die2 = random.randint(1, 6)
+                            # if die1 == die2:
+                            #     # If doubles are rolled, get out of jail
+                            #     in_jail = False
+                            #     turns_in_jail = 0
+                            # else:
+                            #     turns_in_jail += 1
+
+                            # if turns_in_jail >= 3:
+                            #     # After three turns, assume the player pays the $50 fine and gets out of jail
+                            #     in_jail = False
+                            #     turns_in_jail = 0
+                            # if get_out_of_jail_cards > 0:
+                            #     # use the "Get Out of Jail Free" card to avoid staying in jail
+                            #     get_out_of_jail_cards -= 1
+                            #     in_jail = False
+                            
+                        elif die1 == die2: #if doubles are rolled, go to position and reroll
+                            total_moves = die1 + die2
+                            player.position = (player.position + total_moves) % len(locations)
+                            player.moves += total_moves
+                            current_space = list(locations.keys())[player.position]
                             space_counts[current_space] += 1
-                            if get_out_of_jail_cards > 0:
-                                # Use the "Get Out of Jail Free" card to avoid staying in jail
-                                get_out_of_jail_cards -= 1
-                                in_jail = False
                            
                                  
                             
@@ -284,20 +351,72 @@ for _ in range(1000):
                         cc_queue.append(community_chest_card)  # Put the card back to the bottom of the deck
                         print(f"Community Chest Card: {community_chest_card}")
                         if "Go to Jail" in community_chest_card:
-                            in_jail = False
-                            get_out_of_jail_cards = False
+                            if get_out_of_jail_cards > 1:
+                                 get_out_of_jail_cards -= 1
+                                 in_jail = False
+                            else:
+                                in_jail = False
+                            
+                            ''' The code below is for strategy B'''
+                            # in_jail = True
+                            # current_space = "In Jail/Just Visiting"
+                            # space_counts[current_space] += 1
+                            # die1 = random.randint(1, 6)
+                            # die2 = random.randint(1, 6)
+                            # if die1 == die2:
+                            #     # If doubles are rolled, get out of jail
+                            #     in_jail = False
+                            #     turns_in_jail = 0
+                            # else:
+                            #     turns_in_jail += 1
+
+                            # if turns_in_jail >= 3:
+                            #     # After three turns, assume the player pays the $50 fine and gets out of jail
+                            #     in_jail = False
+                            #     turns_in_jail = 0
+                            # if get_out_of_jail_cards > 0:
+                            #     # use the "Get Out of Jail Free" card to avoid staying in jail
+                            #     get_out_of_jail_cards -= 1
+                            #     in_jail = False
+                            
                         if "Get Out of Jail Free" in community_chest_card:
                             get_out_of_jail_cards = True
 
                 elif 500 <= event.pos[0] <= 650 and 800 <= event.pos[1] <= 850:
                     # if not in_jail:  
-                        # Draw a Chance card and process it
+                        # draw a Chance card and process it
                         chance_card = chance_queue.popleft()
                         chance_queue.append(chance_card)  # Put the card back to the bottom of the deck
                         print(f"Chance Card: {chance_card}")
                         if "Go to Jail" in chance_card:
-                            in_jail = False
-                            get_out_of_jail_cards = False
+                            if get_out_of_jail_cards > 1:
+                                 get_out_of_jail_cards -= 1
+                                 in_jail = False
+                            else:
+                                in_jail = False
+                            
+                            ''' The code below is for strategy B'''
+                            # in_jail = True
+                            # current_space = "In Jail/Just Visiting"
+                            # space_counts[current_space] += 1
+                            # die1 = random.randint(1, 6)
+                            # die2 = random.randint(1, 6)
+                            # if die1 == die2:
+                            #     # If doubles are rolled, get out of jail
+                            #     in_jail = False
+                            #     turns_in_jail = 0
+                            # else:
+                            #     turns_in_jail += 1
+
+                            # if turns_in_jail >= 3:
+                            #     # After three turns, assume the player pays the $50 fine and gets out of jail
+                            #     in_jail = False
+                            #     turns_in_jail = 0
+                            # if get_out_of_jail_cards > 0:
+                            #     # use the "Get Out of Jail Free" card to avoid staying in jail
+                            #     get_out_of_jail_cards -= 1
+                            #     in_jail = False
+                            
                         if "Get Out of Jail Free" in chance_card:
                             get_out_of_jail_cards = True
 
@@ -306,10 +425,10 @@ for _ in range(1000):
                 
                 
 
-        # Draw buttons on the screen
+        # draw buttons on the screen
         draw_buttons()
 
-        # Draw text on the screen for all locations, player variables, and buttons
+        # draw text on the screen for locations, variables, and buttons
         draw_text("Position: " + list(locations.keys())[player.position], font, white, screen, 10, 50)
         draw_text("Dice: " + str(die1 + die2), font, white, screen, 10, 150)
         draw_text("Moves: " + str(player.moves), font, white, screen, 10, 250)
@@ -323,8 +442,6 @@ for _ in range(1000):
         draw_text("Spaces and Landings:", smaller_font, white, screen, 350, 50)
         space_counts_text = "\n".join([f"{space}: {count}" for space, count in space_counts.items()])
         draw_text(space_counts_text, smaller_font, white, screen, 350, 100)
-
-        
 
         pygame.display.update()
         clock.tick(fps)
